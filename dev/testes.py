df1f4c7ad0bda6e4c446681dbc781badc22577a2
allen-21 <valteranibal1@gmail.com>
2025-07-21 17:31:49 +0200

    arquivo de testes

---
+++ dev/testes.py
@@ -0,0 +1,10 @@
+from crud import insert_book,insert_user, listar_livros
+
+#crud do livro
+#insert_book("Python", "Anibal", "Plurar", 2020, "484as845648" )
+
+listar_livros()
+
+
+#crud do usuario
+#insert_user("Valter", "Muchanga", "Malhazine", "valteranibal1@gmail.com", "8445484564845" )
